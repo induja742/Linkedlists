e8cc82baf0c781d9c8a96b15ceb7ec7a4d3561bc
Induja Kala <indujakala19@gmail.com>
2021-08-06 20:15:44 -0700

    counts the no of nodes

---
+++ countnodes.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+};
+int  number_of_nodes(Node* n)
+{
+    int count = 0;
+    while(n != NULL) {
+        count++;
+        n = n->next;
+    }
+    return count;
+
+}
+int main(){
+    Node *head = NULL;
+    Node *first = NULL;
+    Node *second = NULL;
+     // allocate 3 nodes in the heap
+    head = new Node();
+    second = new Node();
+    first = new Node();
+    head->data = 1; // assign data in first node
+    head->next = first; // Link first node with
+    // the second node
+ // assign data to second node
+    first->data = 2;
+ 
+    // Link second node with the third node
+    first->next = second;
+
+ 
+    second->data = 3; // assign data to third node
+    second->next = NULL;
+    cout<<number_of_nodes(head);
+ 
+    
+    return 0;
+}
